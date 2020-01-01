74855049a0ef0b1096fcc42e2bc41f8c382a5587
sajicode <sajioloye@gmail.com>
2020-01-02 00:23:30 +0100

    use dynamic coordinates

--- public/js/map.js
+++ public/js/map.js
@@ -5,3 +5,52 @@ var map = new mapboxgl.Map({
 	zoom: 9,
 	center: [ 3.4535067, 6.4502409 ]
 });
+
+// Fetch stores from API
+async function getStores() {
+	const res = await fetch('/api/v1/stores');
+	const data = await res.json();
+
+	const stores = data.data.map((store) => {
+		return {
+			type: 'Feature',
+			geometry: {
+				type: 'Point',
+				coordinates: [ store.location.coordinates[0], store.location.coordinates[1] ]
+			},
+			properties: {
+				storeId: store.storeId,
+				icon: 'shop'
+			}
+		};
+	});
+
+	loadMap(stores);
+}
+
+// Load map with stores
+function loadMap(stores) {
+	map.on('load', function() {
+		map.addLayer({
+			id: 'points',
+			type: 'symbol',
+			source: {
+				type: 'geojson',
+				data: {
+					type: 'FeatureCollection',
+					features: stores
+				}
+			},
+			layout: {
+				'icon-image': '{icon}-15',
+				'icon-size': 1.5,
+				'text-field': '{storeId}',
+				'text-font': [ 'Open Sans Semibold', 'Arial Unicode MS Bold' ],
+				'text-offset': [ 0, 0.9 ],
+				'text-anchor': 'top'
+			}
+		});
+	});
+}
+
+getStores();
